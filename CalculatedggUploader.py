b2d034acc33ddf2a98d2a584f43bd942cb75fd36
Xylot <Joseph@Josephs-MacBook-Air.local>
2018-11-13 12:16:22 -0600

    Added support for mac filepaths

--- CalculatedggUploader.py
+++ CalculatedggUploader.py
@@ -5,13 +5,25 @@ import requests
 import os
 import time
 import enum
+import platform
 from stat import S_ISREG, ST_CTIME, ST_MODE
 from progressbar import AnimatedMarker, Bar, BouncingBar, Counter, ETA, \
     AdaptiveETA, FileTransferSpeed, FormatLabel, Percentage, \
     ProgressBar, ReverseBar, RotatingMarker, \
     SimpleProgress, Timer, UnknownLength
 
-demoPath = "C:/Users/Joseph/Documents/My Games/Rocket League/TAGame/Demos/"
+def assignPathFromOperatingSystem():
+	demoPathWindows = 'C:/Users/Joseph/Documents/My Games/Rocket League/TAGame/Demos/'
+	demoPathMac = '/Users/Joseph/Documents/CalculatedGG-Uploader/Test Files'
+	if platform.system() == 'Windows':
+		return demoPathWindows
+	elif platform.system() == 'Darwin':
+		return demoPathMac
+
+def clearScreen():
+	os.system('cls' if os.name == 'nt' else 'clear')
+
+demoPath = assignPathFromOperatingSystem()
 uploadURL = 'https://calculated.gg/api/upload'
 latestReplayName = ''
 
@@ -34,37 +46,43 @@ class uploadStateDescriptions(enum.Enum):
 	uploadSuccessful = 'Replay successfully uploaded'
 	uploadUnsuccessful = 'Replay was not uploaded successfully'
 
-currentState = uploadState.setup
+def updateCurrentState(state):
+	sys.stdout.write('\r')
+	sys.stdout.flush()
+	sys.stdout.write('Current Status: ' + str(state.name))
+
+#updateCurrentState(uploadState.setup)
 
 def main():
-	currentState = uploadState.setup
+	#updateCurrentState(uploadState.setup)
 	replayFileList = getReplayNames()
 	replayCount = len(replayFileList)
 	latestReplayName = replayFileList[0][1]
 	uploadCount = 0
 
-	os.system('cls')
-	pbar = setupProgressBar(6)	
+	clearScreen()
+	pbar = setupProgressBar(6)
 
 	#for file in pbar((file for file in range(5))):
 	for file in replayFileList[0:5]:
 	#for file in replayFileList:
 		#print(file)
-		currentState = uploadState.openingFile
-		printCurrentState()
+		
+		#updateCurrentState()
 		openReplay = {
 			'replays': open(file[0], 'rb')
 		}
 		latestReplayName = str(file[1])
+		print('\nLatest Replay Uploaded: ' + latestReplayName)
+		updateCurrentState(uploadState.openingFile)
 		uploadReplay(uploadURL, openReplay)
 		uploadCount = uploadCount + 1
-		os.system('cls')
+		clearScreen()
 		pbar.update(uploadCount)
-		print('\nLatest Replay Uploaded: ' + latestReplayName)
 		time.sleep(2)
 
 def setupProgressBar(replayCount):
-	widgets = ['Upload Progress: ', Percentage(), ' ', Bar(marker=RotatingMarker()),' ', ETA(), ' ', Counter(), '\nLatest Replay: ', latestReplayName]
+	widgets = ['Upload Progress: ', Percentage(), ' ', Bar(marker=RotatingMarker()),' ', ETA(), ' ', Counter()]
 	return ProgressBar(widgets=widgets, maxval=replayCount).start()
 
 def getLatestReplay():
@@ -115,20 +133,15 @@ def getReplayNames():
 
 	# return timeListReverse
 
-def printCurrentState():
-	print('\rCurrent Status: ' + str(currentState))
-
 def uploadReplay(uploadURL, replayFile):
 	#print('uploading...')
-	currentState = uploadState.uploadingFile
-	printCurrentState()
+	updateCurrentState(uploadState.uploadingFile)
 	reply = requests.post(uploadURL, files = replayFile)
 
-	currentState = uploadState.interpretingReply
+	updateCurrentState(uploadState.interpretingReply)
 	if not list(reply.json()):
 		message = 'No files uploaded, not a replay'
-		currentState = uploadState.uploadUnsuccessful
-		printCurrentState()
+		updateCurrentState(uploadState.uploadUnsuccessful)
 
 	#if '202' in str(list(reply.json())[0]):
 	if reply.status_code == 202:
@@ -149,18 +162,16 @@ def uploadReplay(uploadURL, replayFile):
 
 	else :
 		message = 'No files uploaded, error ' + reply.status_code
-		currentState = uploadState.uploadUnsuccessful
-		printCurrentState()
+		updateCurrentState(uploadState.uploadUnsuccessful)
 
 def monitorUploadStatus(uploadURL, payload, statusCode):
-	currentState = uploadState.waitingForSuccessfulUpload
-	printCurrentState()
-	while True:		
+	updateCurrentState(uploadState.waitingForSuccessfulUpload)
+	while True:
 		status = requests.get(uploadURL, params = payload)
 		response = str(list(status.json())[0])
 		if checkForResponse(response, 'SUCCESS', 1):
-			currentState = uploadState.uploadSuccessful
-			printCurrentState()
+			updateCurrentState(uploadState.uploadSuccessful)
+			clearScreen()
 			break
 
 def checkForResponse(status, waitCode, sleepAmount):
