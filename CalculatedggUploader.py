4ec6910770a540171050e3cbc3b0c665647957a1
Xylot <Joseph.Meriam@gmail.com>
2018-11-13 10:35:25 -0600

    Revert "Improved output information"
    This reverts commit bbb76410b9f8b11f38a949e184dcc6b3ffbb96b2.

--- CalculatedggUploader.py
+++ CalculatedggUploader.py
@@ -4,133 +4,63 @@ import sys
 import requests
 import os
 import time
-import enum
-from stat import S_ISREG, ST_CTIME, ST_MODE
 from progressbar import AnimatedMarker, Bar, BouncingBar, Counter, ETA, \
     AdaptiveETA, FileTransferSpeed, FormatLabel, Percentage, \
     ProgressBar, ReverseBar, RotatingMarker, \
     SimpleProgress, Timer, UnknownLength
 
-demoPath = "C:/Users/Joseph/Documents/My Games/Rocket League/TAGame/Demos/"
-uploadURL = 'https://calculated.gg/api/upload'
-latestReplayName = ''
-
-
-class uploadState(enum.Enum):
-	setup = 1
-	openingFile = 2
-	uploadingFile = 3
-	interpretingReply = 4
-	waitingForSuccessfulUpload = 5
-	uploadSuccessful = 6
-	uploadUnsuccessful = 7
-
-class uploadStateDescriptions(enum.Enum):
-	setup = 'Initializing'
-	openingFile = 'Opening file'
-	uploadingFile = 'Uploading file'
-	interpretingReply = 'Interpreting reply'
-	waitingForSuccessfulUpload = 'Waiting for replay to be parsed'
-	uploadSuccessful = 'Replay successfully uploaded'
-	uploadUnsuccessful = 'Replay was not uploaded successfully'
-
-currentState = uploadState.setup
+pbar = ProgressBar()
 
 def main():
-	currentState = uploadState.setup
+	replay_file_path = "C:/Users/Joseph/Documents/My Games/Rocket League/TAGame/Demos/7FE4E971472B39F07165C095836F7430.replay"
+	uploadURL = 'https://calculated.gg/api/upload'
+	replays = {}
+	uploadCount = 0
 	replayFileList = getReplayNames()
 	replayCount = len(replayFileList)
-	latestReplayName = replayFileList[0][1]
-	uploadCount = 0
 
-	os.system('cls')
-	pbar = setupProgressBar(6)	
+	widgets = ['Test: ', Percentage(), ' ', Bar(marker=RotatingMarker()),' ', ETA(), ' ', FileTransferSpeed()]
+	pbar = ProgressBar(widgets=widgets, maxval=replayCount).start()
 
 	#for file in pbar((file for file in range(5))):
-	for file in replayFileList[0:5]:
-	#for file in replayFileList:
-		#print(file)
-		currentState = uploadState.openingFile
-		printCurrentState()
+	for file in pbar(replayFileList):
+		print(file)
 		openReplay = {
-			'replays': open(file[0], 'rb')
+			'replays': open(file, 'rb')
 		}
-		latestReplayName = str(file[1])
+		#print('uploading...')
 		uploadReplay(uploadURL, openReplay)
 		uploadCount = uploadCount + 1
-		os.system('cls')
-		pbar.update(uploadCount)
-		print('\nLatest Replay Uploaded: ' + latestReplayName)
+		print("Replays uploaded: " + str(uploadCount) + "  Latest replay: " + x)
 		time.sleep(2)
 
-def setupProgressBar(replayCount):
-	widgets = ['Upload Progress: ', Percentage(), ' ', Bar(marker=RotatingMarker()),' ', ETA(), ' ', Counter(), '\nLatest Replay: ', latestReplayName]
-	return ProgressBar(widgets=widgets, maxval=replayCount).start()
-
-def getLatestReplay():
-	return latestReplayName
-
 def getReplayNames():
-	#Relative or absolute path to the directory
-	dir_path = demoPath
+	demoPath = "C:/Users/Joseph/Documents/My Games/Rocket League/TAGame/Demos/"
+	fileList = [(x[0], time.ctime(x[1].st_ctime)) for x in sorted([(fn, os.stat(fn)) for fn in os.listdir(demoPath)], key = lambda x: x[1].st_ctime)]
+	timeList = []
 
-	#all entries in the directory w/ stats
-	data = (os.path.join(dir_path, fn) for fn in os.listdir(dir_path))
-	data = ((os.stat(path), path) for path in data)
+	for x in fileList:
+		if "2018" in x[1]:
+			timeList.append(x[0])# print(list(reversed(timeList)))
 
-	# regular files, insert creation date
-	data = ((stat[ST_CTIME], path)
-				for stat, path in data if S_ISREG(stat[ST_MODE]))
+	timeListReverse = list(reversed(timeList))# print(onlyfiles)
 
-	replayAttributes = []
-	replayFilesAbsPaths = []
-	replayFileNames = []
+	for x in timeListReverse:
+		if ".replay" not in x:
+			timeListReverse.remove(x)
 
-	for cdate, path in sorted(data):
-		#print(os.path.basename(path))
-		#print(time.ctime(cdate), os.path.basename(path))
-		replayFilesAbsPaths.append(os.path.abspath(path))
-		replayFileNames.append(os.path.basename(path))
-		replayAttributes.append([os.path.abspath(path), os.path.basename(path)])
-
-	for fileName in replayAttributes[0]:
-		if ".replay" not in fileName:
-			replayFilesAbsPaths.remove(fileName)
-
-	for fileName in replayAttributes[1]:
-		if ".replay" not in fileName:
-			replayFilesAbsPaths.remove(fileName)
-
-	return replayAttributes
-
-	# for x in fileList:
-	# 	if "2018" in x[1]:
-	# 		timeList.append(x[0])# print(list(reversed(timeList)))
-
-	# timeListReverse = list(reversed(timeList))# print(onlyfiles)
-
-	# for x in timeListReverse:
-	# 	if ".replay" not in x:
-	# 		timeListReverse.remove(x)
-
-	# return timeListReverse
-
-def printCurrentState():
-	print('\rCurrent Status: ' + str(currentState))
+	return timeListReverse
 
 def uploadReplay(uploadURL, replayFile):
-	#print('uploading...')
-	currentState = uploadState.uploadingFile
-	printCurrentState()
+	print('uploading...')
 	reply = requests.post(uploadURL, files = replayFile)
+	print(reply)
+	print('received reply...')
 
-	currentState = uploadState.interpretingReply
+
 	if not list(reply.json()):
 		message = 'No files uploaded, not a replay'
-		currentState = uploadState.uploadUnsuccessful
-		printCurrentState()
 
-	#if '202' in str(list(reply.json())[0]):
 	if reply.status_code == 202:
 		reply_id = list(reply.json())[0]
 		payload = {
@@ -149,26 +79,23 @@ def uploadReplay(uploadURL, replayFile):
 
 	else :
 		message = 'No files uploaded, error ' + reply.status_code
-		currentState = uploadState.uploadUnsuccessful
-		printCurrentState()
 
 def monitorUploadStatus(uploadURL, payload, statusCode):
-	currentState = uploadState.waitingForSuccessfulUpload
-	printCurrentState()
 	while True:		
 		status = requests.get(uploadURL, params = payload)
-		response = str(list(status.json())[0])
+		response = list(status.json())[0]
 		if checkForResponse(response, 'SUCCESS', 1):
-			currentState = uploadState.uploadSuccessful
-			printCurrentState()
+			print('replay uploaded!')
 			break
 
 def checkForResponse(status, waitCode, sleepAmount):
-	if status == waitCode:
-		return True
-	else:
-		time.sleep(5)
+	if status is not waitCode:
+		time.sleep(sleepAmount)
 		return False
+	else:
+		return True
+
+
 
 if __name__ == '__main__':
 	main()
